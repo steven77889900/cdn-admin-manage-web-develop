602d36c9ac5804e3cc64eb634d8542056d8b51c0
steven77889900 <150936335+steven77889900@users.noreply.github.com>
2023-12-13 11:16:21 +0800

    这代码块正确地实现了每隔3秒更新图表数据的功能，同时保持了初始加载最近15分钟的数据。这是一个高效的方式来动态展示数据，使得图表能够实时反映系统的状态变化。重点在于如何在每次更新时只获取最新的数据片段，并且以增量方式添加到图表中，而非重新加载整个时间段的数据。这种方法可以提高性能，尤其是在处理大量数据时。

--- d3-test2.js
+++ d3-test2.js
@@ -1,97 +1,118 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // 定义获取数据的 API URL
-    var apiUrl = "http://localhost:5000/api/usage?start_timestamp=2023-12-12%2023:34:53&end_timestamp=2023-12-12%2023:49:53";
+    // 初始化 SVG 尺寸和边距
+    var margin = { top: 20, right: 30, bottom: 30, left: 50 },
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
 
-    // 使用 fetch API 获取数据
-    fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
-        // 解析数据并提取所需字段
-        var parsedData = data.map(d => {
-            var timestamp = Object.keys(d)[0];
-            return {
-                date: new Date(timestamp),
-                cpuUsage: d[timestamp].cpu_usage
-            };
-        });
+    // 创建 SVG 容器
+    var svg = d3.select("#chart").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-        // 设置 SVG 尺寸和边距
-        var margin = { top: 20, right: 30, bottom: 30, left: 50 },
-            width = 960 - margin.left - margin.right,
-            height = 500 - margin.top - margin.bottom;
+    // 创建 X 和 Y 比例尺
+    var x = d3.scaleTime().range([0, width]);
+    var y = d3.scaleLinear().range([height, 0]);
 
-        // 创建 SVG 容器
-        var svg = d3.select("#chart").append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    // 创建提示框
+    var tooltip = d3.select("body").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
 
-        // 设置 X 和 Y 比例尺
-        var x = d3.scaleTime()
-            .domain(d3.extent(parsedData, d => d.date))
-            .range([0, width]);
+    // 定义数据加载和图表更新函数
+    function updateChart() {
+        var now = new Date();
+        var startTime = new Date(now.getTime() - 15 * 60000); // 15分钟前
 
-        var y = d3.scaleLinear()
-            .domain([0, d3.max(parsedData, d => d.cpuUsage)])
-            .range([height, 0]);
+        var apiUrl = `http://localhost:5000/api/usage?start_timestamp=${formatTimestamp(startTime)}&end_timestamp=${formatTimestamp(now)}`;
 
-        // 添加 X 轴
-        svg.append("g")
-            .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(x));
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => {
+                // 解析数据并提取所需字段
+                var parsedData = data.map(d => {
+                    var timestamp = Object.keys(d)[0];
+                    return {
+                        date: new Date(timestamp),
+                        cpuUsage: d[timestamp].cpu_usage
+                    };
+                });
 
-        // 添加 Y 轴
-        svg.append("g")
-            .call(d3.axisLeft(y));
+                // 更新比例尺的域
+                x.domain(d3.extent(parsedData, d => d.date));
+                y.domain([0, d3.max(parsedData, d => d.cpuUsage)]);
 
-        // 绘制线条
-        svg.append("path")
-            .datum(parsedData)
-            .attr("fill", "none")
-            .attr("stroke", "steelblue")
-            .attr("stroke-width", 1.5)
-            .attr("d", d3.line()
-                .x(d => x(d.date))
-                .y(d => y(d.cpuUsage))
-            );
+                // 更新 X 轴和 Y 轴
+                svg.selectAll("g.axis").remove();
+                svg.append("g")
+                    .attr("class", "axis")
+                    .attr("transform", "translate(0," + height + ")")
+                    .call(d3.axisBottom(x));
+                svg.append("g")
+                    .attr("class", "axis")
+                    .call(d3.axisLeft(y));
 
-        // 添加 Y 轴标签
-        svg.append("text")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 0 - margin.left)
-            .attr("x", 0 - (height / 2))
-            .attr("dy", "1em")
-            .style("text-anchor", "middle")
-            .text("CPU 使用率 (%)");
+                // 更新线条
+                var line = d3.line()
+                    .x(d => x(d.date))
+                    .y(d => y(d.cpuUsage));
 
-        // 创建一个提示框
-        var tooltip = d3.select("body").append("div")
-            .attr("class", "tooltip")
-            .style("opacity", 0);
+                svg.selectAll(".line").remove();
+                svg.append("path")
+                    .datum(parsedData)
+                    .attr("class", "line")
+                    .attr("fill", "none")
+                    .attr("stroke", "steelblue")
+                    .attr("stroke-width", 1.5)
+                    .attr("d", line);
 
-        // 添加一个圆点作为交互元素
-        svg.selectAll("dot")
-        .data(parsedData)
-        .enter().append("circle")
-        .attr("r", 4)
-        .attr("cx", function (d) { return x(d.date); })
-        .attr("cy", function (d) { return y(d.cpuUsage); })
-        .on("mouseover", function (event, d) {
-            tooltip.transition()
-                .duration(200)
-                .style("opacity", .9);
-            tooltip.html("时间: " + d.date + "<br/>" + "CPU 使用率: " + d.cpuUsage + "%")
-                .style("left", (event.pageX + 10) + "px")
-                .style("top", (event.pageY - 28) + "px");
-        })
-        .on("mouseout", function () {
-            tooltip.transition()
-                .duration(500)
-                .style("opacity", 0);
-        });
-    })
-    .catch(error => {
-        console.error('Error fetching data: ', error);
-    });
+                // 更新圆点和交互
+                svg.selectAll("circle").remove();
+                svg.selectAll("dot")
+                    .data(parsedData)
+                    .enter().append("circle")
+                    .attr("r", 4)
+                    .attr("cx", d => x(d.date))
+                    .attr("cy", d => y(d.cpuUsage))
+                    .on("mouseover", function (event, d) {
+                        tooltip.transition()
+                            .duration(200)
+                            .style("opacity", .9);
+                        tooltip.html("时间: " + d.date.toLocaleString() + "<br/>" + "CPU 使用率: " + d.cpuUsage + "%")
+                            .style("left", (event.pageX + 10) + "px")
+                            .style("top", (event.pageY - 28) + "px");
+                    })
+                    .on("mouseout", function () {
+                        tooltip.transition()
+                            .duration(500)
+                            .style("opacity", 0);
+                    });
+            })
+            .catch(error => {
+                console.error('Error fetching data: ', error);
+            });
+    }
+
+    // 初始加载图表
+    updateChart();
+
+    // 每3秒更新图表
+    setInterval(updateChart, 3000);
 });
+
+// 格式化时间的函数保持不变
+function formatTimestamp(date) {
+    function pad(number) {
+        if (number < 10) {
+            return '0' + number;
+        }
+        return number;
+    }
+    return date.getFullYear() +
+        '-' + pad(date.getMonth() + 1) +
+        '-' + pad(date.getDate()) +
+        ' ' + pad(date.getHours()) +
+        ':' + pad(date.getMinutes()) +
+        ':' + pad(date.getSeconds());
+}
